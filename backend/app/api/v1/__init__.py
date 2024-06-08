688edf2f42e7df6919e8c46b3545974500e822cd
Tianyu Zhang <dowdah@163.com>
2024-06-09 01:36:15 +0800

    financialreport.py -> financial_report.py

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -7,7 +7,7 @@ from .auth import auth_bp
 from .user import user_bp
 from .card import card_bp
 from .transaction import transaction_bp
-from .financialreport import fr_bp
+from .financial_report import fr_bp
 
 v1_bp.register_blueprint(auth_bp, url_prefix='/auth')
 v1_bp.register_blueprint(user_bp, url_prefix='/user')
