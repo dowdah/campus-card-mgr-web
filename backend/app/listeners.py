17a431a2ec72168ad30d362444d7a932716bd401
Tianyu Zhang <dowdah@163.com>
2024-06-08 20:14:30 +0800

    Add listeners.py

---
+++ backend/app/listeners.py
@@ -0,0 +1,19 @@
+from . import db
+from sqlalchemy import event
+from sqlalchemy.orm import Session
+from .models import User, Role
+
+
+def user_before_insert(mapper, connection, target):
+    if target.alternative_id is None:
+        target.alternative_id = User.generate_alternative_id()
+
+
+def user_before_flush(session, flush_context, instances):
+    for instance in session.new:
+        if isinstance(instance, User) and instance.role is None:
+            instance.role = Role.query.filter_by(default=True).first()
+
+
+event.listen(User, 'before_insert', user_before_insert)
+event.listen(Session, 'before_flush', user_before_flush)
