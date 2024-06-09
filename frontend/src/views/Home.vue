59d5937d1de60944ab9e3b32ecd8187809ffec9a
Tianyu Zhang <dowdah@163.com>
2024-06-10 00:57:21 +0800

    Delete redundant styles

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,14 +2,7 @@
 <Dashboard v-if="isAuthenticated"></Dashboard>
   <Login v-else></Login>
 </template>
-<style scoped>
-.dashboard-box {
-  display: flex;
-  width: fit-content;
-  padding: 0 10px; /* 为内部组件分配额外的空间 */
-  margin: 0 auto;
-}
-</style>
+
 <script>
 import Dashboard from '../components/Dashboard.vue';
 import Login from '../components/Login.vue';
